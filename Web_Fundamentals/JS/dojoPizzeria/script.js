ad7779237371970e57b4c550b94ebc776243cdbe
gpapathimiu <gracepapathimiu@yahoo.com>
2023-11-03 21:51:07 +0100

    Dojo Pizzeria

---
+++ Web_Fundamentals/JS/dojoPizzeria/script.js
@@ -0,0 +1,20 @@
+function pizzaOven(crustType, sauceType, cheeses, toppings) {
+    var pizza = {};
+    pizza.crustType = crustType;
+    pizza.sauceType = sauceType;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+
+var pizza1 = pizzaOven("deep dish", "traditional", "mozzarella", ["pepperoni", "sausage"]);
+console.log(pizza1)
+
+var pizza2 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
+console.log(pizza2)
+
+var pizza3 = pizzaOven("thin crust", "panna", "cheddar", ["mushrooms", "spicy salami"]);
+console.log(pizza3)
+
+var pizza4 = pizzaOven("stuffed crust", "pesto", "talagani cheese", ["salami", "tomatoes", "cream cheese", "cheeto dust"])
+console.log(pizza4)
